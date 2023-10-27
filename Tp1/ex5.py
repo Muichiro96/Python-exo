3e6f471c3fbc026a1d65e45533aba51d0a59b289
AFROUKH oussama <65024956+ous47@users.noreply.github.com>
2023-10-27 16:43:30 +0100

    Tp1

---
+++ Tp1/ex5.py
@@ -0,0 +1,6 @@
+n1 = int(input("donnez un nombre entier"))
+n2 = int(input("donnez un nombre entier"))
+if (n1 > 0 & n2 > 0) | (n1 < 0 & n2 < 0):
+    print("le produit est positif")
+else:
+    print("le produit est negatif")
